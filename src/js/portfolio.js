d3640242c8e765bf1b25dac4238b28253a13c2f4
Sarah Hewitt <hello@sarahs.website>
2015-04-17 17:22:04 +0100

    no message

--- src/js/portfolio.js
+++ src/js/portfolio.js
@@ -1,103 +1,80 @@
 $(document).ready(function(){
 
+	//GLOBAL VARIABLES
 	var originalHeight = "22%";
-	var fullHeight = "64%";
-	var smallHeight = "10%";
+	var fullHeight = "77%";
+	var smallHeight = "5%";
 	
+	//PAGE LOAD ANIMATION
 	$(".nameplate").animate({height:"250px",width:"500px",marginTop:"-125px",marginLeft:"-250px"},1500,"easeOutBack",function(){
-		$(".content h1").css("display","block")
-		$(".content h1").animate({top:"40%"},1500, 'easeOutBounce', function(){
-			$(".content p").animate({top:"55%"});
-			$(".content p").fadeIn("1500",function(){
+		$(".content h1").animate({top:"40%",opacity:1},1500, 'easeOutBounce', function(){
+			$(".content p").animate({opacity:1},1500,function(){
 				$(".nameplate").animate({top:"125px"},1500,function(){
-					$(".content h1").animate({top:"35%"},1500);
-					$(".content p").animate({top:"55%"},1500);
+					$(".content h1").animate({top:"30%"},1500);
+					$(".content p").animate({top:"30%",marginTop:"40px"},1500);
 					$(".nameplate").animate({height:originalHeight,width:"100%",marginLeft:"-50%"},1500,"easeOutSine",function(){
-						$(".content h1").css("transform","translate(0,0)");
-						$(".content p").css("transform","translate(0,0)");
-						$(".content h1").animate({left:"0"},1500);
-						$(".content p").animate({left:"0"},1500,function(){
-							$(".contentplate").animate({top:"26%"},1500);
-							$(".contentplate2").animate({top:"52%"},1500);
-							$(".contactplate").animate({top:"78%"},1500);
-						});
+						$(".content h1").addClass("animated");
+						$(".content p").addClass("animated");
+						$(".contentplate").animate({top:"26%"},1500);
+						$(".contentplate2").animate({top:"52%"},1500);
+						$(".contactplate").animate({top:"78%"},1500);
 					});
 				});
 			});
 		});
 	});
 
+	//CLICK INTERACTION FOR WORK PLATE
 	$(".workplate").click(function(){
-		$(".aboutcontent").fadeOut("1500");
-		$(".contactcontent").fadeOut("1500");
-		$(this).animate({height:fullHeight,top:"12%"},1500);
+		$(".aboutcontent, .contactcontent").animate({opacity:0},1500);
+		$(this).animate({height:fullHeight,top:"11%"},1500);
 		$(this).addClass("active");
 		$(".workplate .platetitle h1").animate({top:"5%"},1500,function(){
-			$(".workplate .platetitle span").fadeIn("1500");
+			$(".workcontent li").animate({opacity:1},1500);
 		});
-		$(".nameplate").animate({height:smallHeight},1500);
-		$(".aboutplate").removeClass("active");
-		$(".aboutplate").animate({height:smallHeight,top:"78%"},1500,function(){
-			$(".aboutplate .platetitle h1").animate({top:"50%"});
-			$(".aboutplate .platetitle span").css("display","none");	
+		// nameplate
+		$(".nameplate").animate({height:"10%"},1500);
+		$(".aboutplate").animate({height:smallHeight,top:"89%"},1500,function(){
+			$(".aboutplate").removeClass("active");
 		});
+		$(".aboutplate .platetitle h1").animate({top:"50%"},1500);
 		$(".contactplate").removeClass("active");
-		$(".contactplate").animate({height:smallHeight,top:"90%"},1500,function(){
-			$(".contactplate .platetitle h1").animate({top:"50%"});
-			$(".contactplate .platetitle span").css("display","none");
-		});
+		$(".contactplate").animate({height:smallHeight,top:"95%"},1500);
+		$(".contactplate .platetitle h1").animate({top:"50%"},1500);
 	});
 
+	//CLICK INTERACTION FOR ABOUT PLATE
 	$(".aboutplate").click(function(){
-		$(".contactcontent").fadeOut("1500");
-		$(this).animate({height:fullHeight,top:"24%"},1500);
+		$(".contactcontent, .workcontent").animate({opacity:0},1500);
+		$(this).animate({height:fullHeight,top:"17%"},1500);
 		$(this).addClass("active");
 		$(".aboutplate .platetitle h1").animate({top:"5%"},1500,function(){
-			$(".aboutplate .platetitle span").fadeIn("1500");
-			$(".aboutcontent").fadeIn("1500");
+			$(".aboutcontent").animate({opacity:1},1500);
 		});
-		$(".nameplate").animate({height:smallHeight},1500);
+		$(".nameplate").animate({height:"10%"},1500);
 		$(".workplate").removeClass("active");
-		$(".workplate").animate({height:smallHeight,top:"12%"},1500,function(){
-			$(".workplate .platetitle h1").animate({top:"50%"});
-			$(".workplate .platetitle span").css("display","none");
-		});
+		$(".workplate .platetitle h1").animate({top:"50%"},1500);
+		$(".workplate").animate({height:smallHeight,top:"11%"},1500);
 		$(".contactplate").removeClass("active");
-		$(".contactplate").animate({height:smallHeight,top:"90%"},1500,function(){
-			$(".contactplate .platetitle h1").animate({top:"50%"});
-			$(".contactplate .platetitle span").css("display","none");
-		});
+		$(".contactplate .platetitle h1").animate({top:"50%"},1500);
+		$(".contactplate").animate({height:smallHeight,top:"95%"},1500);
 	});
 
+	//CLICK INTERACTION FOR CONTACT PLATE
 	$(".contactplate").click(function(){
-		$(".aboutcontent").fadeOut("1500");
-		$(this).animate({height:fullHeight,top:"36%"},1500);
+		$(".aboutcontent, .workcontent").animate({opacity:0},1500);
+		$(this).animate({height:fullHeight,top:"23%"},1500);
 		$(this).addClass("active");
 		$(".contactplate .platetitle h1").animate({top:"5%"},1500,function(){
-			$(".contactplate .platetitle span").fadeIn("1500");
-			$(".contactcontent").fadeIn("1500");		
+			$(".contactcontent").animate({opacity:1},1500);		
 		});
-		$(".nameplate").animate({height:smallHeight},1500);
+		$(".nameplate").animate({height:"10%"},1500);
 		$(".workplate").removeClass("active");
-		$(".workplate").animate({height:smallHeight,top:"12%"},1500,function(){
-			$(".workplate .platetitle h1").animate({top:"50%"});
-			$(".workplate .platetitle span").css("display","none");
-
-		});
+		$(".workplate .platetitle h1").animate({top:"50%"},1500);
+		$(".workplate").animate({height:smallHeight,top:"11%"},1500);
 		$(".aboutplate").removeClass("active");
-		$(".aboutplate").animate({height:smallHeight,top:"24%"},1500,function(){
-			$(".aboutplate .platetitle h1").animate({top:"50%"});
-			$(".aboutplate .platetitle span").css("display","none");
-		});
+		$(".aboutplate .platetitle h1").animate({top:"50%"},1500);
+		$(".aboutplate").animate({height:smallHeight,top:"17%"},1500);
 	});
-
-	//Clicking the close button on the plates
-	// $(".contentplate span").click(function(){
-	// 	$(this).fadeOut("1500");
-	// 	$(".workplate").animate({height:originalHeight,top:"26%"},1500);
-	// 	$(".nameplate").animate({height:originalHeight})
-	// 	$(".aboutplate").animate({height:originalHeight,top:"52%"},1500);
-	// 	$(".contactplate").animate({height:originalHeight,top:"78%"},1500);
-	// });
 
 });
